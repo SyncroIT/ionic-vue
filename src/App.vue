f17e53e69c889d6a12a6a4ecd7937b4bcf572790
SyncroIT <kochylo.paolo@gmail.com>
2018-01-31 11:26:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-app>
+    <ion-page class="show-page">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Your application title</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="content" padding>
+        {{ msg }}
+      </ion-content>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'See? It works!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
